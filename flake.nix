331ad575ff225030f9401a604c57ba2b50fc132b
Jörg Thalheim <joerg@thalheim.io>
2026-07-28 11:16:56 +0200

    switch to nix flakes and consolidate CI
    Move the dev shell into flake.nix and expose the Hugo site as a
    package plus flake checks (website build, treefmt via treefmt-nix,
    typos), so 'nix flake check' covers everything CI needs. Themes are
    fetched as flake inputs since submodules are not part of the flake
    source. This lets us collapse the two GitHub workflows into a single
    nix-based one and drop the unused bors config.

---
+++ flake.nix
@@ -0,0 +1,120 @@
+{
+  description = "blog.thalheim.io";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hugo-vitae = {
+      url = "github:datacobra/hugo-vitae";
+      flake = false;
+    };
+    hugo-atom-feed = {
+      url = "github:kaushalmodi/hugo-atom-feed";
+      flake = false;
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      treefmt-nix,
+      hugo-vitae,
+      hugo-atom-feed,
+    }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+      pkgsFor = system: nixpkgs.legacyPackages.${system};
+      treefmtFor =
+        system:
+        treefmt-nix.lib.evalModule (pkgsFor system) {
+          projectRootFile = "flake.nix";
+          programs.prettier.enable = true;
+          settings.formatter.prettier = {
+            options = [
+              "--prose-wrap"
+              "always"
+              "--write"
+            ];
+            # Hugo templates in *.html are not valid HTML for prettier
+            includes = nixpkgs.lib.mkForce [
+              "*.md"
+              "*.yaml"
+              "*.yml"
+            ];
+          };
+          programs.nixfmt.enable = true;
+        };
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          website = pkgs.stdenvNoCC.mkDerivation {
+            name = "blog.thalheim.io";
+            src = self;
+            nativeBuildInputs = [ pkgs.hugo ];
+            buildPhase = ''
+              runHook preBuild
+              rm -rf themes
+              mkdir themes
+              ln -s ${hugo-vitae} themes/hugo-vitae
+              ln -s ${hugo-atom-feed} themes/hugo-atom-feed
+              hugo --minify --destination $out
+              runHook postBuild
+            '';
+            dontInstall = true;
+          };
+          default = self.packages.${system}.website;
+        }
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.bashInteractive
+              pkgs.hugo
+              pkgs.pandoc
+              pkgs.just
+              pkgs.typos
+              (treefmtFor system).config.build.wrapper
+            ];
+          };
+        }
+      );
+
+      formatter = forAllSystems (system: (treefmtFor system).config.build.wrapper);
+
+      checks = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          website = self.packages.${system}.website;
+          formatting = (treefmtFor system).config.build.check self;
+
+          typos = pkgs.runCommand "typos-check" { nativeBuildInputs = [ pkgs.typos ]; } ''
+            typos --config ${self}/typos.toml ${self}/content
+            touch $out
+          '';
+        }
+      );
+    };
+}
